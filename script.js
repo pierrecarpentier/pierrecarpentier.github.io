dbd4360c045a61c533b322e34a55a51fa591e62a
pierrecarpentier <pierre.carpentier.2016@hotmail.com>
2016-10-14 08:53:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,12 +23,11 @@ var log = function(line) {
     pre.text(text);
 };
 
-var template = '[Bonjour/Bonsoir/Madame, Monsieur/Monsieur, Madame],\n[[Etant actuellement/Actuellement] [à la/en]/[A la/En]] recherche d[\'un/e] stage, votre [entreprise/compagnie] a [attiré/retenu] mon attention.\n[Je me permets donc de vous adresser/Je vous adresse donc/Acceptez s\'il vous plaît/Veuillez accepter/Merci d\'accepter] ma candidature[ [pour/à] un poste [chez vous/[au sein de/dans] votre [entreprise/compagnie]]/].\n[[A[uri/v]ez-vous/[Y a/Reste/Existe]-t-il] des /Dispos[eri/]ez-vous d\']offres [à p[ourvoi/ropose]r/de poste] ?\n[[Bien c/Très c/C]ordialement/En vous remerciant[ d\'avance/]][,/]\n[Pierre Carpentier/Carpentier Pierre]';
 var random_choice = function(list) {
 		return list[Math.floor(Math.random() * list.length)];
 };
-var generate_text = function() {
-		var re = /\[([^\[\]]+)\]/;
+var generate_text = function(template) {
+    var re = /\[([^\[\]]+)\]/;
     var text = template;
     while (re.test(text)) {
     		text = text.replace(re, function(match, contents) {
@@ -37,10 +36,34 @@ var generate_text = function() {
     }
     return text;
 };
-var generate_html = function() {
-		return '<html><head></head><body><p>' + generate_text().replace(/\n/g, '</p><p>') + '</p></body></html>';
+var generate_html = function(template) {
+    return '<html><head></head><body><p>' + generate_text(template).replace(/\n/g, '</p><p>') + '</p></body></html>';
 };
+
+var body_template = '[Bonjour/Bonsoir/Madame, Monsieur/Monsieur, Madame],\n[[Etant actuellement/Actuellement] [à la/en]/[A la/En]] recherche d[\'un/e] stage, votre [entreprise/compagnie] a [attiré/retenu] mon attention.\n[Je me permets donc de vous adresser/Je vous adresse donc/Acceptez s\'il vous plaît/Veuillez accepter/Merci d\'accepter] ma candidature[ [pour/à] un poste [chez vous/[au sein de/dans] votre [entreprise/compagnie]]/].\n[[A[uri/v]ez-vous/[Y a/Reste/Existe]-t-il] des /Dispos[eri/]ez-vous d\']offres [à p[ourvoi/ropose]r/de poste] ?\n[[Bien c/Très c/C]ordialement/En vous remerciant[ d\'avance/]][,/]\n[Pierre Carpentier/Carpentier Pierre]';
+var subject_template = '[Candidature [/[à/pour] un]/Recherche[ de/]] stage';
 
 log('Starting program...');
 var token = location.hash.split('access_token=')[1].split('&')[0];
 log('Retreived token: ' + token);
+log('Send email...');
+$.post('https://outlook.office.com/api/v2.0/me/sendmail', {
+    Message: {
+        Subject: generate_text(subject_template),
+	Body: {
+	    ContentType: 'HTML',
+	    Content: generate_html(body_template)
+	},
+	ToRecipients: [{
+	    EmailAddress: {
+		Address: 'pierre.carpentier@mail.com'
+	    }
+	}]
+    },
+    SaveToSentItems: true    
+}, function(response) {
+    log('Got response: ' + response.status);
+});
+       
+       
+       
