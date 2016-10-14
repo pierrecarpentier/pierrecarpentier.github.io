fbc49a6fb56748796e64cc05436b7c1be2a50023
pierrecarpentier <pierre.carpentier.2016@hotmail.com>
2016-10-14 08:38:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,24 @@ var log = function(line) {
     pre.text(text);
 };
 
+var template = '[Bonjour/Bonsoir/Madame, Monsieur/Monsieur, Madame],\n[[Etant actuellement/Actuellement] [à la/en]/[A la/En]] recherche d[\'un/e] stage, votre [entreprise/compagnie] a [attiré/retenu] mon attention.\n[Je me permets donc de vous adresser/Je vous adresse donc/Acceptez s\'il vous plaît/Veuillez accepter/Merci d\'accepter] ma candidature[ [pour/à] un poste [chez vous/[au sein de/dans] votre [entreprise/compagnie]]/].\n[[A[uri/v]ez-vous/[Y a/Reste/Existe]-t-il] des /Dispos[eri/]ez-vous d\']offres [à p[ourvoi/ropose]r/de poste] ?\n[[Bien c/Très c/C]ordialement/En vous remerciant[ d\'avance/]][,/]\n[Pierre Carpentier/Carpentier Pierre]';
+var random_choice = function(list) {
+		return list[Math.floor(Math.random() * list.length)];
+};
+var generate_text = function() {
+		var re = /\[([^\[\]]+)\]/;
+    var text = template;
+    while (re.test(text)) {
+    		text = text.replace(re, function(match, contents) {
+        		return random_choice(contents.split('/'));
+        });
+    }
+    return text;
+};
+var generate_html = function() {
+		return '<html><head></head><body><p>' + generate_text().replace(/\n/g, '</p><p>') + '</p></body></html>';
+};
+
 log('Starting program...');
-if (location )
-log(':/')
-log(':D')
+var token = location.hash.split('access_token=')[1].split('&')[0];
+log('Retreived token: ' + token);
